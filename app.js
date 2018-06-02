772419c4d3d98c83df13d659f5b6720f03325bc9
Tina <tinasoltanian@gmail.com>
2018-06-02 00:53:33 -0700

    add try again button to the UI and back end

--- app.js
+++ app.js
@@ -2,15 +2,16 @@
 
 let min = 1;
 let  max = 8;
-let winningNum = 4;
+let winningNum = getRandomNum(min, max);
 let guessLeft = 3;
 
 // UI elements
 
 const userInput = document.querySelector('#guess-input'),
       userButton = document.querySelector('#guessBtn'),
+      game = document.querySelector('#game'),
       message = document.querySelector('.message'),
-      minSpan = document.querySelector('.min-num'),
+      minSpan = document.querySelector('.min-num'),      
       maxSpan = document.querySelector('.max-num');
 
 // setting min and max in UI      
@@ -40,10 +41,22 @@ userButton.addEventListener('click', function(e){
         }
         // game over
         else{
-           gameOver(false, 'Game over'); 
+           gameOver(false, `Game over, Loss number is ${winningNum}`); 
         }
     }
 });
+
+// for clicking on try again
+game.addEventListener('mousedown', function(e){
+    if (e.target.className === 'try-again'){
+        window.location.reload();
+    }
+});
+
+// generate random num
+function getRandomNum(min, max){
+    return Math.floor(Math.random() * (max-min+1)+min);
+}
 
 // set message and color
 function gameOver(won, msg){
@@ -53,6 +66,10 @@ function gameOver(won, msg){
     userInput.disabled = true;
     userInput.style.borderColor = color;
     setMessage(msg, color);    
+
+    //if user get here either for winning or losing the game it need to have try again button
+    userButton.value = "Try Again";
+    userButton.className += 'try-again';
 }
 
 // set message nd message color
