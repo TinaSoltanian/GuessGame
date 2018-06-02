a519625024fb5a91c216ddb25078a6738344c846
Tina <tinasoltanian@gmail.com>
2018-06-01 23:23:24 -0700

    some code refactoring

--- app.js
+++ app.js
@@ -13,36 +13,49 @@ const userInput = document.querySelector('#guess-input'),
       minSpan = document.querySelector('.min-num'),
       maxSpan = document.querySelector('.max-num');
 
+// setting min and max in UI      
 minSpan.textContent = min;
 maxSpan.textContent = max;      
 
 userButton.addEventListener('click', function(e){
     let guess = parseInt(userInput.value);
 
+    // if the number is bigger than the range
      if (isNaN(guess) || guess > max || guess < min){
         setMessage(`Please enter a number between ${min} and ${max}`,'red');
         return;
     }
 
+    // if user won
     if (guess === winningNum){
-        userInput.disabled = true;
-        userInput.style.borderColor = 'green';
-        setMessage(`${winningNum} is correct, YOU WON!`,'green');
+        gameOver(true, `${winningNum} is correct, YOU WON!`);
     }
     else{
-        if (guessLeft >0    ){
+        // still has tries to go
+        if (guessLeft > 0 ){
             guessLeft--;
             userInput.style.borderColor = 'red';
             userInput.value = '';
             setMessage(`${guess} is incorrect, ${guessLeft} guesses left`,'red');
         }
+        // game over
         else{
-            userInput.disabled = true;
-            setMessage(`Game over`,'red');
+           gameOver(false, 'Game over'); 
         }
     }
 });
 
+// set message and color
+function gameOver(won, msg){
+    let color;
+     won === true ? color = 'green' : color = 'red';
+
+    userInput.disabled = true;
+    userInput.style.borderColor = color;
+    setMessage(msg, color);    
+}
+
+// set message nd message color
 function setMessage(messageStr, color){
     message.textContent = messageStr;
     message.style.color = color;
